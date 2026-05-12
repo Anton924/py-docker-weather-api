59c2cde38e5b1ae361f424df855ed250b57bd80f
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-05-12 23:17:50 +0200

    feat: Add weather api logic and configuration to the dockerignore Dockerfile requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+import pprint
+import os
+
+
+def get_weather(
+        requested_info: str,
+        q_param: str
+) -> None:
+    base_url = "http://api.weatherapi.com/v1"
+
+    pprint.pprint(
+        requests.get(
+            f"{base_url}/{requested_info}"
+            f"?key={os.getenv('API_KEY')}&q={q_param}"
+        ).text
+    )
 
 
 if __name__ == "__main__":
-    get_weather()
+    get_weather("current.json", "Paris")
