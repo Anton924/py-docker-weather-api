bf13b2c9cadc683597eaed7d5529d2aced6edd2c
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-05-13 11:45:02 +0200

    refactor: Add custom ApiError exception & improve exception handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import requests
-import pprint
 import os
+
+
+class ApiError(Exception):
+    def __init__(self, code: int, message: str) -> None:
+        self.code = code
+        self.message = message
+        super().__init__(message)
 
 
 def get_weather(
@@ -8,14 +14,28 @@ def get_weather(
         q_param: str
 ) -> None:
     base_url = "http://api.weatherapi.com/v1"
-
-    pprint.pprint(
-        requests.get(
-            f"{base_url}/{requested_info}"
-            f"?key={os.getenv('API_KEY')}&q={q_param}"
-        ).text
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ApiError(400, "You should provide the API KEY.")
+    response = requests.get(
+        f"{base_url}/{requested_info}",
+        params={"key": api_key, "q": q_param}
     )
+    if response.status_code != 200:
+        data = response.json()
+        print(data)
+        if "error" in data:
+            raise ApiError(
+                data["error"].get("code"),
+                data["error"].get("message")
+            )
+        raise ApiError(response.status_code, "Check your code for errors!")
+
+    print(response.text)
 
 
 if __name__ == "__main__":
-    get_weather("current.json", "Paris")
+    try:
+        get_weather("current.json", "Paris")
+    except ApiError as e:
+        print(f"{e.code} {e.message}")
